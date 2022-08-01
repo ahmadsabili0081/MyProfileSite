7dff51a15c835f3a7d8e0a1f99036cee7b536a3c
Ahmadsabili <ahmadsabili0081@gmail.com>
2022-08-01 19:37:37 +0700

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
+// tabs javascript
+let tab = document.querySelectorAll('.list__portofolio');
+let tabsContents  = document.querySelectorAll('[data-tab-content]');
 
+tab.forEach((item) => {
+  item.addEventListener('click', () => {
+    let target = document.querySelector(item.dataset.tabTarget)
+    tabsContents.forEach((content) => {
+      content.classList.remove('active')
+    })
+    tab.forEach((tabs) => {
+      tabs.classList.remove('active')
+    })
+    item.classList.add('active');
+    target.classList.add('active');
+  })
+});
 // skill bar
 let current = 0;
 let interval = 200;
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded',() => {
   let progressBar = document.querySelectorAll('.progress__bar');
   let valuesBar = ["80%","80%","60%","50%","50%"];
   let colorBar = ['#C2C2C2',"#C2C2C2",'#C2C2C2','#C2C2C2','#C2C2C2'];
   progressBar.forEach((progress,index) =>{
-    progress.style.width = valuesBar[index];
+    progress.style.width = `${valuesBar[index]}`;
     progress.style.backgroundColor = `${colorBar[index]}`
     setInterval(function animation(){
       if(current < parseInt(valuesBar[index])){
@@ -17,12 +33,13 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     }, interval);
   });
-});
+}) 
 // buton show more
 let btnShowMore = document.querySelector('.btn__projectss');
 btnShowMore.addEventListener('click', () =>{
   let hiddenElements = document.querySelectorAll('.row__projects__portofolio .none');
-  let elements = Array.prototype.slice.call(hiddenElements).slice(0,2);
+  console.log(hiddenElements)
+  let elements = Array.prototype.slice.call(hiddenElements).slice(0,1);
   elements.forEach((item,index) => {
     item.classList.add('block')
     item.classList.remove('none');
@@ -36,49 +53,37 @@ btnShowMore.addEventListener('click', () =>{
     btnShowMore.style.display = 'none';
   }
 });
-// small device
-let container = document.querySelector('.container');
-let smallDevice = window.matchMedia("(max-width:320px)");
-smallDevice.addListener(handleDeviceScreen);
-function handleDeviceScreen(e){
-  if(e.matches){
-    container.classList.add('containerElements');
-  }else{
-    container.classList.remove('containerElements');
-  }
-}
 // modall gallery
-let modalGallery = document.getElementById('modal__window');
-let pict = document.getElementsByClassName('images-item')
-let modalOpen = false;
-let clickOutSide = false;
-
-let modalClose = document.getElementById('modal__close');
-modalClose.addEventListener('click', imageClose, false);
-
+let pict = document.getElementsByClassName('images-item');
+let modal = document.querySelector('.modal__gallery');
+let modalOpen = true;
 for(let i = 0; i < pict.length; i++){
-  pict[i].addEventListener('click', imageOpen, false);
+  pict[i].addEventListener('click',openImage,false)
 }
-function imageOpen(i){
-  listenerLaunch()
-  modalGallery.style.display = "block";
-  document.getElementById('modal__pict').innerHTML = '<img src="' + this.src + '">';
-  clickOutSide = false;
+function openImage(){
+  outSideArea();
   modalOpen = true;
+  modal.style.display = "block";
+  document.getElementById('modal__pict').innerHTML = '<img src="' + this.src + '">';
 }
-function imageClose(){
+
+let modalClose = document.querySelector('#modalClose');
+modalClose.addEventListener('click',closeImage,false);
+function closeImage(){
   modalOpen = false;
-  modalGallery.style.display = "none";
+  modal.style.display = "none";
 }
-function listenerLaunch(){
-  modalGallery.addEventListener('click', function(event){
-    clickOutSide = !document.getElementById('modal__pict').getElementsByTagName("img")[0].contains(event.target);
-    if(clickOutSide && modalOpen){
-      modalGallery.style.display = "none";
-      modalOpen = false;
-    }
-  });
+function outSideArea(){
+  modal.addEventListener('click',(e) => {
+    // jika tidak mengambil atau mengklick gambar maka hasilny adalah false negasi yang artinya !false itu adalah true
+    let clickOutSide = !document.getElementById('modal__pict').getElementsByTagName("img")[0].contains(e.target);
+    console.log(clickOutSide)
+   if(modalOpen && clickOutSide){
+    modal.style.display = "none";
+   }
+  })
 }
+console.log(!false)
 // click menu list
 let ul = document.querySelector('ul');
 let li = document.querySelectorAll('li');
@@ -110,4 +115,14 @@ for(let i = 0;  i < notFunctions.length; i++){
   });
 }
 
-
+// small device
+let container = document.querySelector('.container');
+let smallDevice = window.matchMedia("(max-width:320px)");
+smallDevice.addListener(handleDeviceScreen);
+function handleDeviceScreen(e){
+  if(e.matches){
+    container.classList.add('containerElements');
+  }else{
+    container.classList.remove('containerElements');
+  }
+}
